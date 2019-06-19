c93e7dd35a6e62b9a309e6258b4d2acc9e107988
Gabriel Suarez <gabriel.suarez@solarwinds.com>
2019-06-19 13:56:29 +0100

    Added right prefix to the function

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def repo_is_installed(host):
+def test_repo_is_installed(host):
     pkg = host.package("percona-release")
     assert pkg.is_installed
